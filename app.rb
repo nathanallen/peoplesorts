10193717f598110235e0c19966a9f26a1fe5dbf4
nathanallen <thenathanator@gmail.com>
2014-02-06 15:50:41 -0800

    saving progress

--- app.rb
+++ app.rb
@@ -21,6 +21,16 @@ class Main
   end
 end
 
+class ViewController
+  def self.render(title,objects)
+    p title
+    objects.each do |obj|
+      p "#{obj.last_name} #{obj.first_name} #{obj.middle_initial} #{obj.gender} #{obj.date_of_birth} #{obj.favorite_color}"
+    end
+    p ""
+  end
+end
+
 class DSV #delimeter seperated values
 
   def self.parse(filepath)
@@ -53,16 +63,69 @@ class DSV #delimeter seperated values
 
 end
 
-class Person < People_Finder
-  attr_accessor :lastName, :firstName, :middleInitial, :gender, :dateOfBirth, :favoriteColor
+class PeopleSearch
+
+  def self.by_gender #(females before males) then by last name ascending (a-z).
+    by_last_name.sort_by{|person| person.gender}
+  end
+
+  def self.by_birth # - Output 2 – sorted by birth date, ascending (lo-hi).
+    all.sort_by{|person| person.date_of_birth}
+  end
+
+  def self.by_last_name
+    all.sort_by{|person| person.last_name}
+  end
+
+  def self.by_last_name_descending # by last name, descending z-a.
+    by_last_name.reverse
+  end
+
+end
+
+class People < PeopleSearch
+
+  @@people = []
+
+  def initialize(*_)
+    add_person
+  end
+
+  def self.all
+    @@people
+  end
+
+  # def people
+  #   @people ||= []
+  # end
+
+  private
+
+  def add_person
+    @@people << self
+  end
+
+end
+
+class Person < People
+  attr_accessor :last_name, :first_name, :middle_initial, :gender, :date_of_birth, :favorite_color
   
   def initialize(*args)
-    @lastName = args[0]
-    @firstName = args[1]
-    @middleInitial = args[2]
-    @gender = args[3]
-    @dateOfBirth = args[4]
-    @favoriteColor = args[5]
+    @last_name = args[0]
+    @first_name = args[1]
+    @middle_initial = args[2]
+    @gender = standardize_gender(args[3])
+    @date_of_birth = args[4]
+    @favorite_color = args[5]
+    super
+  end
+
+  private
+
+  def standardize_gender(gender)
+    return "Male" if gender == "M"
+    return "FEMALE" if gender == "F"
+    return gender
   end
 
 end
@@ -71,9 +134,10 @@ delimeters = ['comma','pipe','space']
 files = delimeters.map{|delimeter| "sample/#{delimeter}.txt" }
 
 main = Main.new(files)
-output = main.people
 
-p output
+ViewController.render("Output 1", People.by_gender)
+ViewController.render("Output 2", People.by_birth)
+ViewController.render("Output 3", People.by_last_name_descending)
 
 
 # # Sanity Checks
