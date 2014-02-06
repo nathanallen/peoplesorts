ded722893fd4febf5eb3a3f0f5f281989a88d25a
nathanallen <thenathanator@gmail.com>
2014-02-06 14:32:16 -0800

    More 00: Person class to hold parsed people

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
-class DSV #delimeter seperated values
+class Main
+  attr_reader :people
 
-  def self.parse_files(filepaths)
-    filepaths.map do |filepath|
-      parse(filepath)
+  def initialize(files)
+    @people = build_people(files)
+  end
+
+  private
+
+  def build_people(files)
+    rows = load_and_parse(files)
+    rows.map do |row|
+      Person.new(*row)
+    end
+  end
+
+  def load_and_parse(files)
+    files.map do |file|
+      DSV.parse(file)
     end.flatten(1)
   end
+end
+
+class DSV #delimeter seperated values
 
   def self.parse(filepath)
     input = read_file(filepath)
@@ -36,15 +53,34 @@ class DSV #delimeter seperated values
 
 end
 
+class Person < People_Finder
+  attr_accessor :lastName, :firstName, :middleInitial, :gender, :dateOfBirth, :favoriteColor
+  
+  def initialize(*args)
+    @lastName = args[0]
+    @firstName = args[1]
+    @middleInitial = args[2]
+    @gender = args[3]
+    @dateOfBirth = args[4]
+    @favoriteColor = args[5]
+  end
+
+end
+
 delimeters = ['comma','pipe','space']
 files = delimeters.map{|delimeter| "sample/#{delimeter}.txt" }
-output = DSV.parse_files(files)
 
-## Sanity Checks
-## Regex
+main = Main.new(files)
+output = main.people
+
+p output
+
+
+# # Sanity Checks
+# # Regex
 # p "Smith | Steve | D | M | Red | 3-3-1985".split(DSV.regex) == ["Smith", "Steve", "D", "M", "Red", "3-3-1985"]
 # p "Abercrombie, Neil, Male, Tan, 2/13/1943".split(DSV.regex) == ["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"]
 # p "Seles Monica H F 12-2-1973 Black".split(DSV.regex) == ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]
 
-## Read line
+# # Read line
 # p File.read('sample/comma.txt').split(/^/)[0] == "Abercrombie, Neil, Male, Tan, 2/13/1943\n"
