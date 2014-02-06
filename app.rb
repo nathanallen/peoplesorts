22a99d4a1ee1ab6c08e4da71a3658892618ecd17
nathanallen <thenathanator@gmail.com>
2014-02-06 13:48:57 -0800

    OO refactor, introducing DSV class

--- app.rb
+++ app.rb
@@ -1,40 +1,50 @@
-REGEX = %r{
-  \,\s     # comma
-  |        # or
-  \s\|\s   # pipe
-  |        # or else
-  \s       # space
-}x
+class DSV #delimeter seperated values
+
+  def self.parse_files(filepaths)
+    filepaths.map do |filepath|
+      parse(filepath)
+    end.flatten(1)
+  end
+
+  def self.parse(filepath)
+    input = read_file(filepath)
+    parse_rows_and_values(input)
+  end
+
+  def self.regex
+    %r{
+      \,\s     # comma
+      |        # or
+      \s\|\s   # pipe
+      |        # or else
+      \s       # space
+    }x
+  end
+
+  private
+
+  def self.read_file(filepath)
+    File.read(filepath)
+  end
+
+  def self.parse_rows_and_values(input)
+    rows = input.split(/^/)
+    rows.map do |row|
+      row.chomp.split(regex)
+    end
+  end
+
+end
 
 delimeters = ['comma','pipe','space']
-
-def read_file(filename)
-  File.read(filename)
-end
-
-def parse_rows_and_values(input)
-  rows = input.split(/^/)
-  rows.map do |row|
-    row.chomp.split(REGEX)
-  end
-end
-
-def go(delimeters)
-  delimeters.map do |delimiter|
-    filename = "sample/#{delimiter}.txt"
-    input = read_file(filename)
-    parse_rows_and_values(input)
-  end.flatten(1)
-end
-
-p go(delimeters)
-#p delimeters
+files = delimeters.map{|delimeter| "sample/#{delimeter}.txt" }
+output = DSV.parse_files(files)
 
 ## Sanity Checks
 ## Regex
-# p "Smith | Steve | D | M | Red | 3-3-1985".split(REGEX) == ["Smith", "Steve", "D", "M", "Red", "3-3-1985"]
-# p "Abercrombie, Neil, Male, Tan, 2/13/1943".split(REGEX) == ["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"]
-# p "Seles Monica H F 12-2-1973 Black".split(REGEX) == ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]
+# p "Smith | Steve | D | M | Red | 3-3-1985".split(DSV.regex) == ["Smith", "Steve", "D", "M", "Red", "3-3-1985"]
+# p "Abercrombie, Neil, Male, Tan, 2/13/1943".split(DSV.regex) == ["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"]
+# p "Seles Monica H F 12-2-1973 Black".split(DSV.regex) == ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]
 
 ## Read line
 # p File.read('sample/comma.txt').split(/^/)[0] == "Abercrombie, Neil, Male, Tan, 2/13/1943\n"
