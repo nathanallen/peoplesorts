43a6cfab42ca2fe845c1212e8218bfbf42287b41
nathanallen <thenathanator@gmail.com>
2014-02-07 00:22:54 -0800

    fleshing out unit tests

--- app.rb
+++ app.rb
@@ -164,21 +164,30 @@ class Person < People
 
 end
 
+# Driver Code
 delimeters = ['comma','pipe','space']
 files = delimeters.map{|delimeter| "sample/#{delimeter}.txt" }
-
 main = Main.new(files)
+main.display_recordsets
 
-ViewController.render("Output 1", People.by_gender)
-ViewController.render("Output 2", People.by_birth)
-ViewController.render("Output 3", People.by_last_name_descending)
 
 
 # # Sanity Checks
-# # Regex
+
+# # Regex works for all delimeters:
 # p "Smith | Steve | D | M | Red | 3-3-1985".split(DSV.regex) == ["Smith", "Steve", "D", "M", "Red", "3-3-1985"]
 # p "Abercrombie, Neil, Male, Tan, 2/13/1943".split(DSV.regex) == ["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"]
 # p "Seles Monica H F 12-2-1973 Black".split(DSV.regex) == ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]
 
-# # Read line
+# # Lines are read in and split at the correct point:
 # p File.read('sample/comma.txt').split(/^/)[0] == "Abercrombie, Neil, Male, Tan, 2/13/1943\n"
+
+# # Inspected persons match expected output:
+# steve = Person.new("Smith", "Steve", "D", "M", "3-3-1985", "Red")
+# p steve.inspect == "Smith Steve Male 3/3/1985 Red"
+
+# neil = Person.new("Abercrombie", "Neil", "Male", "2/13/1943", "Tan")
+# p neil.inspect == "Abercrombie Neil Male 2/13/1943 Tan"
+
+# # Rendered recordsets match expected output:
+# p main.display_recordsets == File.read('sample/expected-output.txt')
